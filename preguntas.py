6af1536a9096832bb088990b0fd7cf99aaf03ddf
danielvillam <55595656+danielvillam@users.noreply.github.com>
2022-11-21 13:07:52 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -15,29 +15,31 @@ def pregunta_01():
     Complete el código presentado a continuación.
     """
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv(
+        "C:\\Users\\danii\Documents\\GitHub\\lab---regresion-lineal-univariada-danielvillam\\gm_2008_region.csv",
+    )
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = ____[____].____
-    X = ____[____].____
+    y = df["life"].array
+    X = df["fertility"].array
 
     # Imprima las dimensiones de `y`
-    print(____.____)
+    print(y.shape)
 
     # Imprima las dimensiones de `X`
-    print(____.____)
+    print(X.shape)
 
     # Transforme `y` a un array de numpy usando reshape
-    y_reshaped = y.reshape(____, ____)
+    y_reshaped = y.reshape(len(y), -1)
 
     # Trasforme `X` a un array de numpy usando reshape
-    X_reshaped = X.reshape(____, ____)
+    X_reshaped = X.reshape(len(X), -1)
 
     # Imprima las nuevas dimensiones de `y`
-    print(____.____)
+    print(y_reshaped.shape)
 
     # Imprima las nuevas dimensiones de `X`
-    print(____.____)
+    print(X_reshaped.shape)
 
 
 def pregunta_02():
@@ -47,22 +49,24 @@ def pregunta_02():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv(
+        "C:\\Users\\danii\Documents\\GitHub\\lab---regresion-lineal-univariada-danielvillam\\gm_2008_region.csv",
+    )
 
     # Imprima las dimensiones del DataFrame
-    print(____.____)
+    print(df.shape)
 
     # Imprima la correlación entre las columnas `life` y `fertility` con 4 decimales.
-    print(____)
+    print(df["life"].corr(df["fertility"]).round(4))
 
     # Imprima la media de la columna `life` con 4 decimales.
-    print(____)
+    print(df["life"].mean().round(4))
 
     # Imprima el tipo de dato de la columna `fertility`.
-    print(____)
+    print(type(df["fertility"]))
 
     # Imprima la correlación entre las columnas `GDP` y `life` con 4 decimales.
-    print(____)
+    print(df["GDP"].corr(df["life"]).round(4))
 
 
 def pregunta_03():
@@ -72,35 +76,37 @@ def pregunta_03():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv(
+        "C:\\Users\\danii\Documents\\GitHub\\lab---regresion-lineal-univariada-danielvillam\\gm_2008_region.csv",
+    )
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = ____
+    X_fertility = df["fertility"].array
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = ____
+    y_life = df["life"].array
 
     # Importe LinearRegression
-    from ____ import ____
+    from sklearn.linear_model import LinearRegression
 
     # Cree una instancia del modelo de regresión lineal
-    reg = ____
+    reg = LinearRegression()
 
     # Cree El espacio de predicción. Esto es, use linspace para crear
     # un vector con valores entre el máximo y el mínimo de X_fertility
-    prediction_space = ____(
-        ____,
-        ____,
-    ).reshape(____, _____)
+    prediction_space = np.linspace(
+        X_fertility.min(),
+        X_fertility.max(),
+    ).reshape(len(X_fertility), -1)
 
     # Entrene el modelo usando X_fertility y y_life
-    reg.fit(____, ____)
+    reg.fit(X_fertility, y_life)
 
     # Compute las predicciones para el espacio de predicción
     y_pred = reg.predict(prediction_space)
 
     # Imprima el R^2 del modelo con 4 decimales
-    print(____.score(____, ____).round(____))
+    print(linearRegression.score(X_fertility, y_pred).round(4))
 
 
 def pregunta_04():
@@ -145,3 +151,6 @@ def pregunta_04():
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
     rmse = np.sqrt(____(____, ____))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
+
+if __name__ == "__main__":
+    print(pregunta_02())
